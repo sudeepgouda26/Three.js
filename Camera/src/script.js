b16bbad2a913aaecf6e5c80b2d485bef8a38281d
sudeepgouda26 <sudeepgouda26@gmail.com>
2025-05-03 01:13:58 +0530

    using cos and sin

--- Camera/src/script.js
+++ Camera/src/script.js
@@ -55,8 +55,11 @@ renderer.setSize(sizes.width, sizes.height)
 
 function tick (){
     renderer.render(scene, camera)
-      camera.position.x =cursor.x *10
-      camera.position.y =cursor.y*10
+    //   camera.position.x =cursor.x *10
+    //   camera.position.y =cursor.y*10
+    camera.position.x =Math.sin(cursor.x *Math.PI *2) *3
+    camera.position.z =Math.cos(cursor.x *Math.PI *2) *3
+    camera.position.y = cursor.y *4
       camera.lookAt(mesh.position)
    
     window.requestAnimationFrame(tick)
